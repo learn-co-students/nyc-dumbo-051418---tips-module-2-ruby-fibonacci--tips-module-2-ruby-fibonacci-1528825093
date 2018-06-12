0379b45abd179afcd44d14373b743e8255e51767
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-12 13:57:26 -0400

    Did some fibonacci-ing

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -4,6 +4,20 @@ require 'pry-byebug'
 
 def fibonacci(n)
   # your code here :)
+  if n == 1
+    return 1
+  elsif n == 2
+    return 1
+  else
+    counter = 3
+  end
+
+  fib_array = [0,1,1]
+  while counter <= n
+    fib_array << (fib_array[counter-1] + fib_array[counter-2])
+    counter += 1
+  end
+  return fib_array.last
 end
 
 Pry.start
