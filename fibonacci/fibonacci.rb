00fba9dc17904c224afa0e538c1d2ad6c94e0299
Sammy Akharaz <ketrixz@gmail.com>
2018-06-12 14:52:24 -0400

    done

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,9 +1,27 @@
 require 'pry-byebug'
 # Pry is included in case you'd like to run this file and test it,
 # but you may remove it if you like.
-
+# class Fibonacci
+#
+# end
 def fibonacci(n)
-  # your code here :)
+  first_num = 0
+  second_num = 1
+
+  if n==1
+    return first_num
+  elsif n==2
+    return second_num
+  else
+    result = 0
+    (n-2).times do
+      result = first_num + second_num
+      first_num = second_num
+      second_num = result
+
+    end
+    return result
+  end
 end
 
-Pry.start
+fibonacci(3)
