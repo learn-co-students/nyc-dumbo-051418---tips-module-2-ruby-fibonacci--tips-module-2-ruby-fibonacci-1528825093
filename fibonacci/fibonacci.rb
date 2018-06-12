6bf3e4fa1c50edb5e4a5f7bcada62e7d46930a35
github username <github email address>
2018-06-12 13:57:34 -0400

    added solution

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -2,8 +2,16 @@ require 'pry-byebug'
 # Pry is included in case you'd like to run this file and test it,
 # but you may remove it if you like.
 
+n = gets.chomp.to_i
 def fibonacci(n)
   # your code here :)
+  if n == 1
+    1
+  elsif n == 2
+    1
+  elsif n > 2
+    fibonacci(n-1) + fibonacci(n-2)
+  end
 end
 
 Pry.start
